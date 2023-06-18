01393a571234a52c14e384f815995ce4541cc137
smartraunak <raunak5690@gmail.com>
2023-06-18 11:48:52 +0530

    req

--- question_recommender/question_recommender/settings.py
+++ question_recommender/question_recommender/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4q=6w(z5qk1nt7o$la!jbv+oa1=&=&z3j_jywhfws46=@3#u)*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
